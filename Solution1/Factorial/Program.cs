d61f579518a4cb2bd327d2879a277dadcd45cce1
Lucija Ulaga <lucija.ulaga@fer.hr>
2016-10-31 00:11:34 +0100

    Zad 7 i 8, v.1

--- Solution1/Factorial/Program.cs
+++ Solution1/Factorial/Program.cs
@@ -3,13 +3,76 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Factorial
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            // Main method is the only method that
+            // can ’t be marked with async .
+            // What we are doing here is just a way for us to simulate
+            // async - friendly environment you usually have with
+            // other .NET application types ( like web apps , win apps etc .)
+            // Ignore main method , you can just focus on
+            //LetsSayUserClickedAButtonOnGuiMethod() as a
+            // first method in call hierarchy .
+            var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
+            Console.Read();
+        }
+
+        private static void LetsSayUserClickedAButtonOnGuiMethod()
+        {
+            var result = GetTheMagicNumber();
+            Console.WriteLine(result);
+        }
+
+        private static int GetTheMagicNumber()
+        {
+            return IKnowIGuyWhoKnowsAGuy();
+        }
+
+        private static int IKnowIGuyWhoKnowsAGuy()
+        {
+            return IKnowWhoKnowsThis(10) + IKnowWhoKnowsThis(5);
+        }
+
+        private static int IKnowWhoKnowsThis(int n)
+        {
+            return FactorialDigitSum(n).Result;
+        }
+
+
+        /*public static void Main(string[] args)
+        {
+            Task<int> task = FactorialDigitSum(20);
+            
+            Console.WriteLine(task.Result);
+            Console.ReadLine();
+        }*/
+
+        public static async Task<int> FactorialDigitSum(int n)
+        {
+            int sum = 0;
+
+            BigInteger fac = await Task.Run(() => Factorial(n));
+
+            while (fac > 0)
+            {
+                sum += (int)(fac % 10);
+                fac /= 10;
+            }
+            return sum;
+        }
+
+        public static BigInteger Factorial(int n)
+        {
+            BigInteger fac = 1;
+            for (uint i = 2; i <= n; i++)
+                fac *= i;
+            return fac;
         }
     }
 }
