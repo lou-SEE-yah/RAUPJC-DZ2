0762ddfc69229c2bae8f8b6c231130761afaad58
Lucija Ulaga <lucija.ulaga@fer.hr>
2016-10-30 19:34:48 +0100

    Zadatak 4, v.1

--- Solution1/LINQIzrazi/Program.cs
+++ Solution1/LINQIzrazi/Program.cs
@@ -8,9 +8,36 @@ namespace LINQIzrazi
 {
     class Program
     {
+        public static void Example1()
+        {
+            var list = new List<Student>()
+            {
+                new Student (" Ivan ", jmbag :" 001234567 ")
+            };
+            var ivan = new Student(" Ivan ", jmbag: " 001234567 ");
+            
+            bool anyIvanExists = list.Any(s => s == ivan);
+            Console.WriteLine(anyIvanExists);
+            Console.ReadLine();
+        }
+
+        public static void Example2()
+        {
+            var list = new List<Student>()
+            {
+                new Student (" Ivan ", jmbag :" 001234567 "),
+                new Student (" Ivan ", jmbag :" 001234567 ")
+            };
+            
+            var distinctStudents = list.Distinct().Count();
+            Console.WriteLine(distinctStudents);
+            Console.ReadLine();
+        }
+
+
         static void Main(string[] args)
         {
-            int[] integers = new[] {1, 2, 2, 2, 3, 3, 4, 5};
+            int[] integers = new[] { 1, 2, 2, 2, 3, 3, 4, 5 };
             string[] strings =
             integers.GroupBy(s => s)
                 .Select(s => "Broj " + s.Key.ToString() + " ponavlja se " + s.Count().ToString() + " puta")
@@ -20,7 +47,9 @@ namespace LINQIzrazi
             Console.WriteLine(strings[2]);
             Console.WriteLine(strings[3]);
             Console.WriteLine(strings[4]);
-            Console.ReadKey();
+            Console.ReadLine();
+            Example1();
+            Example2();
         }
     }
 }
---
+++ Solution1/LINQIzrazi/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQIzrazi
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Jmbag { get; set; }
+        public Gender Gender { get; set; }
+
+        public Student(string name, string jmbag)
+        {
+            Name = name;
+            Jmbag = jmbag;
+        }
+
+
+        public static bool operator ==(Student student1, Student student2)
+        {
+            if (object.ReferenceEquals(student1, null) || object.ReferenceEquals(student2, null))
+                return false;
+            return student1.Equals(student2);
+        }
+
+        public static bool operator !=(Student student1, Student student2)
+        {
+            if (object.ReferenceEquals(student1, null) || object.ReferenceEquals(student2, null))
+                return false;
+            return !student1.Equals(student2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var student = obj as Student;
+            if (object.ReferenceEquals(student, null))
+                return false;
+            if (this.Name.Equals(student.Name) && this.Gender.Equals(student.Gender) && this.Jmbag.Equals(student.Jmbag))
+                return true;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Gender.GetHashCode() + this.Jmbag.GetHashCode() + this.Name.GetHashCode();
+        }
+    }
+
+    public enum Gender
+    {
+        Male, Female
+    }
+}
