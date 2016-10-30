4df21d083c8a5be8ad17ac899d4261b586aa4f60
Lucija Ulaga <lucija.ulaga@fer.hr>
2016-10-30 14:43:05 +0100

    Prvi zadatak, v.1

---
+++ Solution1/Models/TodoItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime DateCompleted { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public TodoItem(string text)
+        {
+            Id = Guid.NewGuid(); // Generates new unique identifier
+            Text = text;
+            IsCompleted = false;
+            DateCreated = DateTime.Now; // Set creation date as current time
+        }
+
+        public void MarkAsCompleted()
+        {
+            if (!IsCompleted)
+            {
+                IsCompleted = true;
+                DateCompleted = DateTime.Now;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as TodoItem;
+            if (item == null) return false;
+            return this.Id.Equals(item.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ Solution1/Models/TodoRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interfaces;
+using Models;
+using zad2;
+
+namespace Repositories
+{
+    /// <summary >
+    /// Class that encapsulates all the logic for accessing TodoTtems .
+    /// </ summary >
+    public class TodoRepository : ITodoRepository
+    {
+        /// <summary >
+        /// Repository does not fetch todoItems from the actual database ,
+        /// it uses in memory storage for this excersise .
+        /// </ summary >
+        private readonly IGenericList<TodoItem> _inMemoryTodoDatabase;
+        public TodoRepository(IGenericList<TodoItem> initialDbState = null)
+        {
+            if (initialDbState != null)
+            {
+                _inMemoryTodoDatabase = initialDbState;
+            }
+            else
+            {
+                _inMemoryTodoDatabase = new GenericList<TodoItem>();
+            }
+            // Shorter way to write this in C# using ?? operator :
+            // _inMemoryTodoDatabase = initialDbState ?? new List < TodoItem >() ;
+            // x ?? y -> if x is not null , expression returns x. Else y.
+        }
+
+        public void Add(TodoItem todoItem)
+        {
+            if (_inMemoryTodoDatabase.Where(s => s.Equals(todoItem)).FirstOrDefault() != null)
+            {
+                throw new DuplicateTodoItemException("duplicate id: {" + todoItem.Id + "}");
+            }
+            _inMemoryTodoDatabase.Add(todoItem);
+        }
+
+        public TodoItem Get(Guid todoId)
+        {
+            TodoItem item = _inMemoryTodoDatabase.Where(s => s.Id.Equals(todoId)).FirstOrDefault();
+            return item;
+
+        }
+
+        public List<TodoItem> GetActive()
+        {
+            List<TodoItem> list = _inMemoryTodoDatabase.Where(s => s.IsCompleted == false).ToList();
+            return list;
+        }
+
+        public List<TodoItem> GetAll()
+        {
+            List<TodoItem> list = _inMemoryTodoDatabase.OrderByDescending(s => s.DateCreated).ToList();
+            return list;
+        }
+
+        public List<TodoItem> GetCompleted()
+        {
+            List<TodoItem> list = _inMemoryTodoDatabase.Where(s => s.IsCompleted == true).ToList();
+            return list;
+        }
+
+        public List<TodoItem> GetFiltered(Func<TodoItem, bool> filterFunction)
+        {
+            List<TodoItem> list = _inMemoryTodoDatabase.Where(s => filterFunction(s)==true).ToList();
+            return list;
+        }
+
+        public bool MarkAsCompleted(Guid todoId)
+        {
+            TodoItem item = _inMemoryTodoDatabase.Where(s => s.Id.Equals(todoId)).FirstOrDefault();
+            if (item == null) return false;
+            item.MarkAsCompleted();
+            return true;
+        }
+
+        public bool Remove(Guid todoId)
+        {
+            TodoItem item = _inMemoryTodoDatabase.Where(s=> s.Id.Equals(todoId)).FirstOrDefault();
+            if (item == null)
+                return false;
+            _inMemoryTodoDatabase.Remove(item);
+            return true;
+        }
+
+        public void Update(TodoItem todoItem)
+        {
+            if (_inMemoryTodoDatabase.Contains(todoItem))
+            {
+                var toUpdate = _inMemoryTodoDatabase.Single(x => x.Id.Equals(todoItem.Id));
+                toUpdate = todoItem;
+            }
+            else
+                _inMemoryTodoDatabase.Add(todoItem);
+            
+        }
+    }
+}
